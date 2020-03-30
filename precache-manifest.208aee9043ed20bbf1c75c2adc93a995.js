7d863b9203bf8793429550fd5f3d15407c2c535c
pratik-pdw <pratik.wadekar2810@gmail.com>
2020-03-30 23:08:24 +0530

    Updates

---
+++ precache-manifest.208aee9043ed20bbf1c75c2adc93a995.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3ad54f48f52d0cefbcbb99cf7897968",
+    "url": "/react-astrology/index.html"
+  },
+  {
+    "revision": "f451fa121359f98c50a9",
+    "url": "/react-astrology/static/css/2.a92cf616.chunk.css"
+  },
+  {
+    "revision": "f451fa121359f98c50a9",
+    "url": "/react-astrology/static/js/2.a4e4f76f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-astrology/static/js/2.a4e4f76f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cf6ba2f8b7dd81aa6d2",
+    "url": "/react-astrology/static/js/main.ad02fe3e.chunk.js"
+  },
+  {
+    "revision": "c94c378a754b772fb10c",
+    "url": "/react-astrology/static/js/runtime-main.d5fbf7b4.js"
+  },
+  {
+    "revision": "fe6620be5c0a442843e4dd4951a78bf3",
+    "url": "/react-astrology/static/media/constellations.fe6620be.png"
+  }
+]);
